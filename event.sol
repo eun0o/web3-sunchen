a35d6bf351431d29ea0b2329d640c056e5324c4e
eun0o <52726626+eun0o@users.noreply.github.com>
2019-10-29 15:22:05 +0900

    update - indexed

--- event.sol
+++ event.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.5.0;
 
 contract Event {
     
-    event EventCall(address who);
+    event EventCall(address indexed who);
     
     function call() public {
         emit EventCall(msg.sender);
